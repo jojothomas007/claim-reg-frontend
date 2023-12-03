83bda63cc5334ab2fd974d0e4ac10e6a6245e989
testName <test@gmail.com>
2023-12-03 20:01:17 +0530

    To run from mobile

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -1,5 +1,6 @@
 class Constants {
-  static String baseurl = 'http://localhost:8080';
+  // static String baseurl = 'http://localhost:8080';
+  static String baseurl = 'https://claim-reg.azurewebsites.net';
   static const double confidenceThreshold = 0.90;
   static const String dateFormat = 'dd-MM-yyyy';
 }
